3ce7dc15d6ed7038681bac4082f3e796fcc37d7b
heiwa4126 <heiwa4126@gmail.com>
2024-01-09 13:25:08 +0900

    Add "jump"

--- Assets/Scripts/Objects.cs
+++ Assets/Scripts/Objects.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Objects : MonoBehaviour
 {
+	private Rigidbody rb;
+	private float initialY;
+	[SerializeField] private float lowestY = 0.1f;
+	[SerializeField] private float jumpForce = 5.0f;
+	[SerializeField] private float turnY = 0.5f;
+
 	// Start is called before the first frame update
 	void Start()
 	{
+		rb = GetComponent<Rigidbody>();
+		initialY = transform.position.y;
+	}
 
+	public void Jump()
+	{
+		rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		transform.Rotate(0, 0.5f, 0);
+		transform.Rotate(0, turnY, 0);
+
+		if (transform.position.y - initialY < lowestY && Input.GetKeyDown(KeyCode.Space))
+			Jump();
 	}
 }
