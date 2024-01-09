6fbd7e770533cb25f2d01d4ef458eb0c174dd04e
heiwa4126 <heiwa4126@gmail.com>
2024-01-09 15:34:19 +0900

    Update README

--- Assets/Scripts/MenuSceneManager.cs
+++ Assets/Scripts/MenuSceneManager.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuSceneManager : MonoBehaviour
 {
+	[SerializeField] private TextMeshProUGUI promptText;
+
 	// Start is called before the first frame update
 	void Start()
 	{
-
+		StartCoroutine(BlinkPrompt());
 	}
 
 	// Update is called once per frame
@@ -19,4 +22,16 @@ public class MenuSceneManager : MonoBehaviour
 			SceneManager.LoadScene(1);
 		}
 	}
+
+	IEnumerator BlinkPrompt()
+	{
+		while (true)
+		{
+			promptText.gameObject.SetActive(true);
+			yield return new WaitForSeconds(1);
+			promptText.gameObject.SetActive(false);
+			yield return new WaitForSeconds(0.2f);
+		}
+	}
+
 }
--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 
 This is an Unity project for
 [Submission: Programming theory in action - Unity Learn](https://learn.unity.com/tutorial/submission-programming-theory-in-action?uv=2022.3&labelRequired=true&pathwayId=5f7e17e1edbc2a5ec21a20af&missionId=5f779f1eedbc2a00201f3e5e#60b7bfe3edbc2a0021c7601b)
+
+```text
+Programming Theory Project
+
+It's NOT GAME. Just for tutorial.
+
+See details at https://learn.unity.com/tutorial/submission-programming-theory-in-action
+GitHub: https://github.com/heiwa4126/unity-junior-programmer-programming-theory-project
+```
